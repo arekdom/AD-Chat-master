8fc684b20ac7a24b42c1f757ccf85833d5f3a8c3
Arek <arek@magnat.local>
2018-03-16 15:03:14 +0000

    Initial Commit

---
+++ AD Chat/WelcomeViewController.swift
@@ -0,0 +1,28 @@
+//
+//  WelcomeViewController.swift
+//   AD Chat
+//
+//  Created by Arkadiusz Domanski
+//  Copyright (c) 2018 ADAppStudio. All rights reserved.
+//
+//  This is the welcome view controller - the first thign the user sees
+//
+
+import UIKit
+
+
+
+class WelcomeViewController: UIViewController {
+
+   
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+}
